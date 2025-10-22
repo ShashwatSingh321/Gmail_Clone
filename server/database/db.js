4e4363bfd6f35e3166d7c042150feaa1cc13b6a8
ShashwatSingh321 <shashwatsingh73557@gmail.com>
2025-10-22 09:05:41 +0530

    Update DB_URI to use environment variables

--- server/database/db.js
+++ server/database/db.js
@@ -8,7 +8,7 @@ const PASSWORD = process.env.DB_PASSWORD;
 
 const Connection = () => {
     // const DB_URI = `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.afvw91v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-    const DB_URI = `mongodb+srv://skil173557:<password>@cluster0.afvw91v.mongodb.net/gmailclone?retryWrites=true&w=majority&appName=Cluster0`;
+    const DB_URI = `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.afvw91v.mongodb.net/gmailclone?retryWrites=true&w=majority&appName=Cluster0`;
     
     try {
         mongoose.connect(DB_URI, { useNewUrlParser: true });
